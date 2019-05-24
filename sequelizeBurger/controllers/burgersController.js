d913fcc1ad8f68409a58a1a29377672c37b9150a
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-05-24 19:54:49 -0400

    Added get and post routes to capture the data entered in the body. Also, required express

--- sequelizeBurger/controllers/burgersController.js
+++ sequelizeBurger/controllers/burgersController.js
@@ -1,13 +1,24 @@
 var express = require("express");
 
 var router = express.Router();
-var burger = require("../models/burger.js");
+// edit burger model to match sequelize
+var db = require("../models/");
 
 router.get("/api/burgers", function(req, res) {
-  // express callback response by calling burger.selectAllBurger
-  burger.all(function(burgerData) {
-    res.json(burgerData);
-  });
+  db.Burger.findAll()
+    .then(function(burgerData) {
+      res.json(burgerData);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+  db.Burger.create({
+    burger_name: req.body.burger_name
+  })
+
+    .then(function(result) {
+      res.json({  id:result.insertId  });
+    });
 });
 
 // post route
