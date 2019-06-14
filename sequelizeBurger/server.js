c935f137d2a80b499f4b0eb173b88b22e89db25f
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-06-14 10:25:58 -0400

    Remove scroll bar with overflow-y property

--- sequelizeBurger/server.js
+++ sequelizeBurger/server.js
@@ -6,7 +6,7 @@ var app = express();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 5000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
